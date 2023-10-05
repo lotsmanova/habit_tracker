9ca995f12f971ac9e2f30923c185efa4b8668942
violetta <lotsmanovavioletta@gmail.com>
2023-10-05 18:57:39 +0300

    fixed model Habits

--- tracker/models.py
+++ tracker/models.py
@@ -4,13 +4,23 @@ from users.models import NULLABLE
 
 
 class Habits(models.Model):
+    FREQUENCY_CHOICES = (
+        ('one daily', 'Ежедневно'),
+        ('two daily', 'Раз в два дня'),
+        ('three daily', 'Раз в три дня'),
+        ('four daily', 'Раз в четыре дня'),
+        ('five daily', 'Раз в пять дней'),
+        ('six daily', 'Раз в шесть дней'),
+        ('seven daily', 'Раз в семь дней'),
+    )
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
     place = models.CharField(max_length=150, verbose_name='место исполнения привычки')
     time = models.TimeField(verbose_name='время выполнения привычки')
     action = models.CharField(max_length=150, verbose_name='действие привычки')
     is_good_habit = models.BooleanField(verbose_name='признак приятной привычки', default=False, **NULLABLE)
     related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='связанная привычка', **NULLABLE)
-    frequency = models.CharField(max_length=100, verbose_name='периодичность', default='Ежедневно')
+    frequency = models.CharField(max_length=100, choices=FREQUENCY_CHOICES, verbose_name='периодичность', default='Ежедневно')
     award = models.CharField(max_length=200, verbose_name='вознаграждение', **NULLABLE)
     time_to_complete = models.TimeField(verbose_name='время на выполнение')
     is_public = models.BooleanField(verbose_name='признак публизности', default=False)
